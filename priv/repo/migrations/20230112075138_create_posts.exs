d170e801365e90b6a62166f0b315db80ba93b6be
candidate-Tc3MmFmMmJ <candidate-tc3mmfmmmj@example.com>
2023-01-12 11:01:03 +0300

    Some useful stuff for Post migration

--- priv/repo/migrations/20230112075138_create_posts.exs
+++ priv/repo/migrations/20230112075138_create_posts.exs
@@ -4,9 +4,9 @@ defmodule Blog.Repo.Migrations.CreatePosts do
   def change do
     create table(:posts, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :title, :string
-      add :body, :text
-      add :user_id, references(:users, on_delete: :nothing, type: :binary_id)
+      add :title, :string, null: false
+      add :body, :text, null: false
+      add :user_id, references(:users, on_delete: :delete_all, type: :binary_id), null: false
 
       timestamps()
     end
